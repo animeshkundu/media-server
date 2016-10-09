f052075056dd4a1a48e5dba183f760f8eae296a8
Agix <anik.edu@gmail.com>
2016-10-10 02:09:27 +0530

    HTTP Media Server for VLC | Initial commit
    Signed-off-by: Agix <anik.edu@gmail.com>

---
+++ README.md
@@ -0,0 +1,40 @@
+HTTP Media Server for VLC
+=========================
+
+Me and my friends needed to watch movies and listen to songs wirelessly from a desktop which had all the hard disks attached to it. SMB, AFP were too much of a hassle to set up and not to mention slow, jittery performance. I am looking at you Mac OS. ( SMB 1 in 2016 ?) Hence the python script.
+
+Shows vlc playlist directly in vlc. So that you can click and play :). Both random and incremental seek in vlc is supported. Playing directly in the browser is supported too.
+
+I tried to minimize depedencies as much as possible. But I just couldn't do with netifaces. 
+
+Have fun!
+
+
+Installation
+------------
+pip install -r requirements.txt
+
+
+Usage
+-----
+video.py [-h] [--directory DIRECTORY] [--port PORT] [--foreground]
+                [--stop] [--restart] [--pid PID]
+
+
+optional arguments:
+
+  -h, --help            show this help message and exit
+
+  --directory DIRECTORY, -d DIRECTORY
+                        Directory where the media is located
+
+  --port PORT, -p PORT  Port the Media Server will bind to
+
+  --foreground, -f      Start the Media Server in the foreground
+
+  --stop                Stop the Media Server.
+
+  --restart             Restart the Media Server
+
+  --pid PID             Location to store the pid file
+
---
+++ video.py
@@ -0,0 +1,388 @@
+#! /usr/bin/env python
+
+import sys
+import json
+import os
+import re
+import time
+import atexit
+import ConfigParser
+import cgi
+import threading
+import socket
+import errno
+import glob
+import argparse
+import netifaces
+import BaseHTTPServer
+import SimpleHTTPServer
+
+from signal import SIGTERM 
+from SocketServer import ThreadingMixIn
+from zipfile import ZipFile
+from urlparse import urlparse, parse_qs
+from urllib import urlopen, quote, unquote
+from posixpath import normpath
+from cStringIO import StringIO
+from os.path import (join, exists, dirname, abspath, isabs, sep, walk, splitext,
+    isdir, basename, expanduser, split, splitdrive)
+from os import makedirs, unlink, getcwd, chdir, curdir, pardir, rename, fstat
+from shutil import copyfileobj, copytree
+from netifaces import interfaces, ifaddresses
+
+
+DATA_DIR = getcwd()
+
+class ThreadingHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    pass
+
+
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    serve_path = DATA_DIR
+
+    def do_GET(self):
+        if self.path.endswith('/vlc'):
+            f = self.generate_playlist(self.serve_path)
+            self.copyfile(f, self.wfile)
+            f.close()
+            return
+
+        self.range_from, self.range_to = self._get_range_header()
+        if self.range_from is None:
+            return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+        print 'range request', self.range_from, self.range_to
+
+        f = self.send_range_head()
+        if f:
+            self.copy_file_range(f, self.wfile)
+            f.close()
+
+    def copy_file_range(self, in_file, out_file):
+        in_file.seek(self.range_from)
+        left_to_copy = 1 + self.range_to - self.range_from
+        buf_length = 64*1024
+        bytes_copied = 0
+        while bytes_copied < left_to_copy:
+            read_buf = in_file.read(min(buf_length, left_to_copy))
+            if len(read_buf) == 0:
+                break
+            out_file.write(read_buf)
+            bytes_copied += len(read_buf)
+        return bytes_copied
+
+    def send_range_head(self):
+        path = self.translate_path(self.path)
+        f = None
+        if isdir(path):
+            if not self.path.endswith('/'):
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = join(path, index)
+                if exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+
+        if not exists(path) and path.endswith('/data'):
+            if exists(path[:-5]):
+                path = path[:-5]
+
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+
+        if self.range_from is None:
+            self.send_response(200)
+        else:
+            self.send_response(206)
+
+        self.send_header("Content-type", ctype)
+        fs = fstat(f.fileno())
+        file_size = fs.st_size
+        if self.range_from is not None:
+            if self.range_to is None or self.range_to >= file_size:
+                self.range_to = file_size-1
+            self.send_header("Content-Range",
+                             "bytes %d-%d/%d" % (self.range_from,
+                                                 self.range_to,
+                                                 file_size))
+            self.send_header("Content-Length", 
+                             (1 + self.range_to - self.range_from))
+        else:
+            self.send_header("Content-Length", str(file_size))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def list_directory(self, path):
+        try:
+            list = os.listdir(path)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        f = StringIO()
+        displaypath = cgi.escape(unquote(self.path))
+        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
+        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
+        f.write("<hr>\n<ul>\n")
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+            f.write('<li><a href="%s">%s</a>\n'
+                    % (quote(linkname), cgi.escape(displayname)))
+        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-type", "text/html; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
+
+    def generate_playlist(self, path) :
+        media_list = [os.path.join(root, i) for root, dirs, files in os.walk(path) for i in files if self.is_video_file(i)]
+        url = "http://%s:%d/" % (get_ip_addr(), get_port())
+        length = len(path)
+        f = StringIO()
+        f.write("#EXTM3U\n")
+
+        for track in media_list:
+            basename = os.path.basename(track)
+            track = track[length+1:]
+            track = url + quote(track)
+            f.write("#EXTINF" + ":-1" + "," + basename + "\n" + track + "\n")
+
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-type", "application/mpegurl; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
+
+    def translate_path(self, path):
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        path = normpath(unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = self.serve_path
+        for word in words:
+            drive, word = splitdrive(word)
+            head, word = split(word)
+            if word in (curdir, pardir): continue
+            path = join(path, word)
+        return path
+
+    def _get_range_header(self):
+        range_header = self.headers.getheader("Range")
+        if range_header is None:
+            return (None, None)
+        if not range_header.startswith("bytes="):
+            print "Not implemented: parsing header Range: %s" % range_header
+            return (None, None)
+        regex = re.compile(r"^bytes=(\d+)\-(\d+)?")
+        rangething = regex.search(range_header)
+        if rangething:
+            from_val = int(rangething.group(1))
+            if rangething.group(2) is not None:
+                return (from_val, int(rangething.group(2)))
+            else:
+                return (from_val, None)
+        else:
+            print 'CANNOT PARSE RANGE HEADER:', range_header
+            return (None, None)
+
+    def is_video_file(self, filename):
+        global video_file_extensions
+        return True if filename.endswith((video_file_extensions)) else False
+
+
+class Daemon:
+
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+    
+    def daemonize(self):
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+    
+        #os.chdir("/") 
+        os.setsid() 
+        os.umask(0) 
+    
+        try: 
+            pid = os.fork() 
+            if pid > 0:
+                sys.exit(0) 
+        except OSError, e: 
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1) 
+    
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+    
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile,'w+').write("%s\n" % pid)
+    
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+        
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+    
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return
+
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def run(self):
+	start()
+
+args = None
+
+def get_server(port=1149, next_attempts=0, serve_path=None):
+    Handler = RequestHandler
+    if serve_path:
+        Handler.serve_path = serve_path
+    while next_attempts >= 0:
+        try:
+            httpd = ThreadingHTTPServer(("", port), Handler)
+            return httpd
+        except socket.error as e:
+            if e.errno == errno.EADDRINUSE:
+                next_attempts -= 1
+                port += 1
+            else:
+                raise
+
+def get_ip_addr():
+    ips = [netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr'] for iface in netifaces.interfaces() if netifaces.AF_INET in netifaces.ifaddresses(iface)]
+    for ip in ips :
+        if '127.0.0.1' not in ip :
+            return ip
+
+def get_port() :
+    global args
+    return args.port
+
+def print_banner() :
+    global args
+    print "HTTP Media Server running on %r [ %s ]" % (sys.platform, sys.version_info)
+    print "Serving at http://%s:%d\n" % (get_ip_addr(), get_port())
+    print "Run on the client :  vlc http://%s:%d/vlc" % (get_ip_addr(), get_port())
+
+def start() :
+    global args
+    serve_path = abspath(args.directory)
+    httpd = get_server(port=args.port, serve_path=serve_path)
+    httpd.serve_forever()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="HTTP Media Server for VLC")
+    parser.add_argument("--directory", "-d", default=".", help="Directory where the media is located")
+    parser.add_argument("--port", "-p", default=1149, help="Port the Media Server will bind to")
+    parser.add_argument("--foreground", "-f", action="store_true", help="Start the Media Server in the foreground")
+    parser.add_argument("--stop", action="store_true", help="Stop the Media Server.")
+    parser.add_argument("--restart", action="store_true", help="Restart the Media Server")
+    parser.add_argument("--pid", default="/tmp", help="Location to store the pid file")
+
+    global args
+    args = parser.parse_args()
+   
+    if args.foreground :
+        print_banner()
+        start()
+    else :
+        daemon = Daemon(args.pid + "/py_video_server.pid")
+        if args.stop :
+            print "Stopping Media Server running at http://%s:%d" % (get_ip_addr(), get_port())
+            daemon.stop()
+        elif args.restart :
+            print "Restarting Media Server running at http://%s:%d" % (get_ip_addr(), get_port())
+            daemon.restart()
+        else :
+            print_banner()
+            daemon.start()
+
+
+
+
+
+
+
+video_file_extensions = ( '.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf', '.aec', '.aep', '.aepx', '.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb', '.avc', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik', '.bin', '.bix', '.bmk', '.bnp', '.box', '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine', '.cip', '.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v', '.dat', '.dav', '.dce', '.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb', '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss', '.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr', '.dzm', '.dzp', '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject', '.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp', '.h264', '.hdmov', '.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism', '.ismc', '.ismv', '.iva', '.ivf', '.ivr', '.ivs', '.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g', '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg', '.m1v', '.m21', '.m21', '.m2a', '.m2p', '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv', '.mj2', '.mjp', '.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov', '.movie', '.mp21', '.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf', '.mpg', '.mpg2', '.mpgindex', '.mpl', '.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc', '.mvd', '.mve', '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv', '.nvc', '.ogm', '.ogv', '.ogx', '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist', '.plproj', '.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr', '.pxv', '.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd', '.rmd', '.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt', '.scc', '.scm', '.scm', '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi', '.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf', '.swi', '.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trp', '.ts', '.tsp', '.ttxt', '.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg','.vem', '.vep', '.vf', '.vft', '.vfw', '.vfz', '.vgz', '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7', '.vpj', '.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx', '.wot', '.wp3', '.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog', '.yuv', '.zeg', '.zm1', '.zm2', '.zm3', '.zmv' )
+
+if __name__ == "__main__" :
+    main()
